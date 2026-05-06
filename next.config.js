d7197b8fa4577f7651ee3cdec9818f699fe59977
q965786x <kasatka74@yandex.ru>
2026-05-06 22:02:09 +0700

    Rename StepAerobics.png to Step.png

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Включаем статический экспорт
+  
   images: {
     unoptimized: true, // Отключаем оптимизацию изображений Next.js
   },
