445394cc1935de44d7755078bc6c93c95beeda9f
q965786x <kasatka74@yandex.ru>
2026-05-07 20:42:34 +0700

    Откорректирован файл not-found, внесены корректировки в файл со стилями страницы [lessonId], на логотип повешен клик.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,19 +3,19 @@ import styles from './not-found.module.css';
 
 export default function NotFoundPage() {
     return (
-        <div className={styles.notFound}>
+      <div className={styles.notFound}>
         <div className={styles.container}>
-        <h1 className={styles.title}>404</h1>
-        <h2 className={styles.subtitle}>Страница не найдена</h2>
-        <p className={styles.description}>
-          Возможно, она была удалена
-          <br />
-          или перенесена на другой адрес
-        </p>
-        <Link href="/workout/main" className={styles.homeLink}>
-          Вернуться на главную
-        </Link>
+          <h1 className={styles.title}>404</h1>
+          <h2 className={styles.subtitle}>Страница не найдена</h2>
+          <p className={styles.description}>
+            Возможно, она была удалена
+            <br />
+            или перенесена на другой адрес
+          </p>
+          <Link href="/workout/main" className={styles.homeLink}>
+            Вернуться на главную
+          </Link>
+        </div>
       </div>
-    </div>
     );
 }
